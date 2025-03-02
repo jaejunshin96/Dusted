e4d14c4f88b6be3b0529f254fcf4da2a09d1869f
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-02 17:04:36 +1030

    feat(users): add users app with basic setup

---
+++ backend/users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=150, default="", verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=150, default="", verbose_name=_("Last Name"))
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
