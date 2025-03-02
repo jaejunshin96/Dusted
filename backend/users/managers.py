78f176bca3e77703692ee747531c79f2514b0540
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-02 18:02:39 +1030

    feat(users): add restful setup

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -6,7 +6,7 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, first_name, last_name, password, **extra_fields):
+    def create_user(self, email, username, first_name, last_name, password, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
@@ -17,12 +17,12 @@ class CustomUserManager(BaseUserManager):
         if not last_name:
             raise ValueError(_("The Last Name must be set"))
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user = self.model(email=email, username=username, first_name=first_name, last_name=last_name, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+    def create_superuser(self, email, username, first_name, last_name, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -34,4 +34,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, first_name=first_name, last_name=last_name, **extra_fields)
+        return self.create_user(email, password, username, first_name, last_name, **extra_fields)
