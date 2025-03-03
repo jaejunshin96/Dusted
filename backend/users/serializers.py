f19497ef9c4d66e22d02817798c182acbf8464b8
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-03 18:37:52 +1030

    feat(user): add register serializer and apiview

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,4 +4,31 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "email", "first_name", "last_name", "is_staff", "is_active", "date_joined"]
+        fields = ["id", "email", 'username', "first_name", "last_name", "is_staff", "is_active", "date_joined"]
+
+class CustomUserRegisterationSerializer(serializers.ModelSerializer):
+	password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+	password2 = serializers.CharField(max_length=68, min_length=6, write_only=True)
+
+	class Meta:
+		model = CustomUser
+		fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 'password2']
+
+	# we need compare the two passwords the user provides
+	def validate(self, attrs):
+		password = attrs.get('password', '')
+		password2 = attrs.get('password2', '')
+
+		if password != password2:
+			raise serializers.ValidationError("passwords do not match")
+		return attrs
+
+	def create(self, validated_data):
+		user = CustomUser.objects.create_user(
+			email = validated_data['email'],
+			username = validated_data['username'],
+			first_name = validated_data['first_name'],
+			last_name = validated_data['last_name'],
+			password = validated_data['password'],
+		)
+		return user
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserDetailAPIView
+from .views import UserDetailAPIView, RegisterUserView
 
 urlpatterns = [
 	path('user/<str:username>/', UserDetailAPIView.as_view(), name='user-detail'),
+	path('register/', RegisterUserView.as_view(), name='register-user'),
 ]
 
