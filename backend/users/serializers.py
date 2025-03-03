efdbf1af01478ef501debb15986703de90b7d2e3
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-03 23:10:11 +1030

    feat(uesr): add login and logout features but seem a bit off with blacklist

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from .models import CustomUser
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ["id", "email", 'username', "first_name", "last_name", "is_staff", "is_active", "date_joined"]
 
-class CustomUserRegisterationSerializer(serializers.ModelSerializer):
+class UserRegisterationSerializer(serializers.ModelSerializer):
 	password = serializers.CharField(max_length=68, min_length=6, write_only=True)
 	password2 = serializers.CharField(max_length=68, min_length=6, write_only=True)
 
@@ -29,6 +32,55 @@ class CustomUserRegisterationSerializer(serializers.ModelSerializer):
 			username = validated_data['username'],
 			first_name = validated_data['first_name'],
 			last_name = validated_data['last_name'],
-			password = validated_data['password'],
 		)
+		user.set_password(validated_data['password'])
+		user.save()
 		return user
+
+
+class LoginSerializer(serializers.Serializer):
+	email = serializers.EmailField()
+	username = serializers.CharField(max_length=150, read_only=True)
+	password = serializers.CharField(max_length=68, write_only=True)
+	first_name = serializers.CharField(max_length=150, read_only=True)
+	last_name = serializers.CharField(max_length=150, read_only=True)
+	access_token = serializers.CharField(max_length=255, read_only=True)
+	refresh_token = serializers.CharField(max_length=255, read_only=True)
+
+	class Meta:
+		model = CustomUser
+		fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 'access_token', 'refresh_token']
+
+	def validate(self, attrs):
+		email = attrs.get('email')
+		password = attrs.get('password')
+		request = self.context.get('request')
+
+		user = authenticate(request=request, email=email, password=password)
+		if not user:
+			raise AuthenticationFailed(f"invalid credentials try again")
+
+		user_tokens = user.tokens()
+
+		return {
+			'id': user.id,
+			'email': user.email,
+			'username': user.username,
+			'first_name': user.first_name,
+			'last_name': user.last_name,
+			'access_token': str(user_tokens.get('access')),
+			'refresh_token': str(user_tokens.get('refresh'))
+		}
+
+class LogoutSerializer(serializers.Serializer):
+	refresh = serializers.CharField()
+
+	def validate(self, attrs):
+		self.token = attrs['refresh']
+		return attrs
+
+	def save(self, **kwargs):
+		try:
+			RefreshToken(self.token).blacklist()
+		except TokenError:
+			raise serializers.ValidationError("Invalid token")
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from .models import CustomUser
-from .serializers import CustomUserSerializer, CustomUserRegisterationSerializer
+from .serializers import UserSerializer, UserRegisterationSerializer, LoginSerializer
 
 class UserDetailAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -11,17 +11,32 @@ class UserDetailAPIView(APIView):
     def get(self, request, username):
         try:
             user = CustomUser.objects.get(username=username)
-            serializer = CustomUserSerializer(user)
+            serializer = UserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except CustomUser.DoesNotExist:
-            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"Error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class RegisterUserView(APIView):
 	permission_classes = [AllowAny]
 
 	def post(self, request):
-		serializer = CustomUserRegisterationSerializer(data=request.data)
+		serializer = UserRegisterationSerializer(data=request.data)
 		if serializer.is_valid(raise_exception=True):
 			serializer.save()
-			return Response({'data': serializer.data}, status=status.HTTP_201_CREATED)
-		return Response({'message': "ERROR with registration view"}, serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response({'Error': "registration view"}, serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'Error': "login view"}, serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
