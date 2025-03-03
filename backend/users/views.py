6294bf708fb83eb33563acacf9e92a4be5b5d522
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-03 18:37:52 +1030

    feat(user): add register serializer and apiview

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, CustomUserRegisterationSerializer
 
 class UserDetailAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -15,3 +15,13 @@ class UserDetailAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+class RegisterUserView(APIView):
+	permission_classes = [AllowAny]
+
+	def post(self, request):
+		serializer = CustomUserRegisterationSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			serializer.save()
+			return Response({'data': serializer.data}, status=status.HTTP_201_CREATED)
+		return Response({'message': "ERROR with registration view"}, serializer.errors, status=status.HTTP_400_BAD_REQUEST)
