57d4ef8255d88eeb004261d58610525f073b416d
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-04 19:36:59 +1030

    feat(docker): add frontend docker and django secret to env now it can be public

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 from django.conf import settings
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-57^9p*52urdkeh=5rj#6$muz-=0-sen!y6b--9f4j^m4fr&rda'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
