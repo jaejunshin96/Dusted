1b8a404502c0b3c86f30ef44a6cb944204c0a4e2
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-07 19:01:08 +1030

    feat(postgres): add postgres db and config and modify makefile for django container migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,9 +109,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'default': {
+		"ENGINE": os.getenv('POSTGRES_ENGINE'),
+        "NAME": os.getenv('POSTGRES_DB'),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": os.getenv('POSTGRES_HOST'),
+        "PORT": os.getenv('POSTGRES_PORT'),
     }
 }
 
