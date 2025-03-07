8f6a1d6f00921497f821f4ea9f35acbfb7b9eb5a
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-07 16:34:22 +1030

    feat(uesrs): add email verify

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserDetailAPIView, RegisterUserView, LoginView, LogoutView
+from .views import UserDetailAPIView, RegisterUserView, VerifyEmail, LoginView, LogoutView
 
 urlpatterns = [
 	path('user/<str:username>/', UserDetailAPIView.as_view(), name='user-detail'),
 	path('register/', RegisterUserView.as_view(), name='register-user'),
+	path('verify-email/', VerifyEmail.as_view(), name='verify-email'),
 	path('login/', LoginView.as_view(), name='login'),
 	path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework import status
+from rest_framework import generics, status
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import CustomUser
 from .serializers import UserSerializer, UserRegisterationSerializer, LoginSerializer, LogoutSerializer
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 
 class UserDetailAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -17,14 +21,33 @@ class UserDetailAPIView(APIView):
             return Response({"Error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class RegisterUserView(APIView):
-	permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
-	def post(self, request):
-		serializer = UserRegisterationSerializer(data=request.data)
-		if serializer.is_valid(raise_exception=True):
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response({'Error': "registration view"}, serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = UserRegisterationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            user_data = serializer.data
+            user = CustomUser.objects.get(email=user_data['email'])
+            token = RefreshToken.for_user(user).access_token
+
+            current_site = get_current_site(request).domain
+            relative_link = reverse('verify-email')
+            absurl = 'http://' + current_site + relative_link + "?token=" + str(token)
+            email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
+            data = {'email_body': email_body, 'to_email': user.email,
+                    'email_subject': 'Verify your email'}
+
+            Util.send_email(data)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'Error': "registration view"}, serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
