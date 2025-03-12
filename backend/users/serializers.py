6083e1de2366c2de345f5da3256d7d050d67a77f
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-12 14:33:10 +0900

    feat(users): add reset password by email serializer, view, urls

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from .models import CustomUser
+from django.utils.encoding import smart_bytes, smart_str
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -85,3 +88,42 @@ class LogoutSerializer(serializers.Serializer):
 			RefreshToken(self.token).blacklist()
 		except TokenError:
 			raise serializers.ValidationError("Invalid token")
+
+class RequestPasswordResetEmailSerializer(serializers.Serializer):
+	email = serializers.EmailField()
+
+	redirect_url = serializers.CharField(max_length=500, required=False)
+
+	class Meta:
+		fields = ['email', 'redirect_url']
+
+class SetNewPasswordSerializer(serializers.Serializer):
+	password = serializers.CharField(min_length=6, max_length=68, write_only=True)
+	password2 = serializers.CharField(min_length=6, max_length=68, write_only=True)
+	token = serializers.CharField(min_length=1, write_only=True)
+	uidb64 = serializers.CharField(min_length=1, write_only=True)
+
+	class Meta:
+		fields = ['password', 'password2', 'token', 'uidb64']
+
+	def validate(self, attrs):
+		try:
+			password = attrs.get('password')
+			password2 = attrs.get('password2')
+			token = attrs.get('token')
+			uidb64 = attrs.get('uidb64')
+
+			id = smart_str(urlsafe_base64_decode(uidb64))
+			user = CustomUser.objects.get(id=id)
+
+			if password != password2:
+				raise serializers.ValidationError({'Error': 'Passwords do not match'})
+
+			if not PasswordResetTokenGenerator().check_token(user, token):
+				raise AuthenticationFailed('The reset link is invalid', 401)
+
+			user.set_password(password)
+			user.save()
+			return user
+		except Exception as e:
+			raise AuthenticationFailed('The reset link is invalid', 401)
