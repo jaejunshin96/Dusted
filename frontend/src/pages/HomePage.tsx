45b5ea859f588f73de1398b8cd3f5a75b25f2afa
Jaejun Shin <jaeshinkorean@gmail.com>
2025-03-17 17:33:59 +0900

    feat(frontend): add google login button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import GoogleLoginButton from "./GoogleLoginButton";
 
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to Dusted</h1>
+      <h1>Welcome to Dusty</h1>
       <p>Please log in or register to continue.</p>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
+      <div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <div>
+          <GoogleLoginButton />
+        </div>
+      </GoogleOAuthProvider>
     </div>
   );
 };
