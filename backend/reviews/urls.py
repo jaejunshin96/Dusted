ca4533776be43f5292f139e44ea87e427d70c24e
Jaejun Shin <jaeshinkorean@gmail.com>
2025-04-03 23:14:30 +0900

    add default image for non image movies and check for review completion

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views	import ReviewsList
+from .views	import ReviewsList, ReviewView
 
 urlpatterns = [
 	path('reviews/', ReviewsList.as_view(), name='reviews-list'),
+    path("reviews/status/", ReviewView.as_view(), name="review"),
 ]
