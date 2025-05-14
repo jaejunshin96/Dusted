7a1968ab7d8f3dc73f609917566a5be171ec303c
Jaejun Shin <jaeshinkorean@gmail.com>
2025-05-14 18:13:59 +0900

    add blacklist cleanup and edit view height

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -181,6 +181,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "TOKEN_BLACKLIST_PRUNE_INTERVAL": 60 * 60 * 24,
 }
 
 CACHES = {
